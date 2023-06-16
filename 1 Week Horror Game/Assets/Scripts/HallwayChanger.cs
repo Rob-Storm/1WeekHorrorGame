16c554fc130e3d172e2ac2615a434e50a865b87d
The1Wolfcast <the1wolfcast@gmail.com>
2023-06-15 20:50:15 -0500

    Added tooltips to newly created prefabs

--- 1 Week Horror Game/Assets/Scripts/HallwayChanger.cs
+++ 1 Week Horror Game/Assets/Scripts/HallwayChanger.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class HallwayChanger : MonoBehaviour
 {
+    [Tooltip("The objects you want to have a chance of spawning (AFFECTS ALL OF THEM)")]
     public GameObject[] element;
 
+    [Tooltip("The fraction chance the object stays (i.e 1/2 would be a %50 chance it remains or 1/3 would be a %33 chance)")]
     public int chance;
 
     void Start()
